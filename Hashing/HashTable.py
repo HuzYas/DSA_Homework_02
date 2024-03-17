bbe1f7fe595e53f24b5631bccae164c9298205bf
FaizaQureshi93 <40027357+FaizaQureshi93@users.noreply.github.com>
2024-03-17 08:55:43 +0500

    Add files via upload

---
+++ Hashing/HashTable.py
@@ -0,0 +1,25 @@
+def create_hashtable(size): # returns tuple(list,list)
+    pass
+
+def resize_hashtable(hashtable,size,increase): #return hashtable,size
+   pass
+def hash_function(key,size): #returns integer (Address)
+    pass
+
+def collision_resolver(key,oldAddress,size): #returns integer (Address)
+   pass
+
+def put(hashtable,key, data,size): #return hashtable,size
+   pass   
+def loadFactor(hashtable,size): # returns a float - Loadfactor of hashtable
+    pass
+
+def Update(hashtable,key, columnName, data,size,collision_path,opNumber): # returns Nothing, prints 'record Updated'
+    pass
+        
+    
+def get(hashtable,key,size,collision_path,opNumber): # returns dictionary
+    pass
+        
+def delete(hashtable, key, size,collision_path,opNumber): #returns hashtable, size, prints a msg  'Item Deleted'
+   pass
